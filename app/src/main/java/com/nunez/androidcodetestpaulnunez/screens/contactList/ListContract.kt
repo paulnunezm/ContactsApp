74e8779fd0045ae567ce9566d32a7b6b671c3549
Paul Nunez <paulnunezm@gmail.com>
2017-09-21 11:45:40 -0400

    Edit: updated ListContract method names and signatures to be more accurate with the behaviour needed.

--- app/src/main/java/com/nunez/androidcodetestpaulnunez/screens/contactList/ListContract.kt
+++ app/src/main/java/com/nunez/androidcodetestpaulnunez/screens/contactList/ListContract.kt
@@ -7,27 +7,28 @@ import io.reactivex.Single
 interface ListContract {
 
     interface View {
-        fun contactClickListener()
-        fun contactLongClickListener()
+        fun addContactClickListener()
+        fun contactClickListener(id: String)
+        fun contactLongClickListener(id: String)
         fun toolbarClickListener()
         fun showContacts(contacts: List<Contact>)
-        fun showContactDetails(id: String)
         fun showOptionsModalBottomSheet(id: String)
-        fun showErrorMessage()
+        fun showErrorMessage(message: String)
         fun dismissModal()
         fun goToAddActivity()
-        fun goToEditActivity(contactId:String)
+        fun goToEditActivity(contactId: String)
         fun goToDetailsActivity(contactId: String)
+        fun goToSearchActivity(contactId: String)
     }
 
     interface Presenter {
-        fun showErrorMessage()
         fun requestContacts()
-        fun addContactClicked()
+        fun onAdContactClicked()
         fun onContactCliked(id: String)
         fun onContactLongCliked(id: String)
         fun onContactDeleteClicked(id: String)
         fun onContactEditClicked(id: String)
+        fun onToolbarClicked()
     }
 
     interface Interactor {
