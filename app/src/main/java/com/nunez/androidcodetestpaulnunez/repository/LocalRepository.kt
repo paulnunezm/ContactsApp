6b6b97f96b494b247a56da1acac3f883e5fd1bf4
Paul Nunez <paulnunezm@gmail.com>
2017-09-22 00:29:22 -0400

    Added: Local repository implementation.

---
+++ app/src/main/java/com/nunez/androidcodetestpaulnunez/repository/LocalRepository.kt
@@ -0,0 +1,74 @@
+package com.nunez.androidcodetestpaulnunez.repository
+
+import com.nunez.androidcodetestpaulnunez.entities.Contact
+import com.vicpin.krealmextensions.*
+import io.reactivex.Completable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.realm.Realm
+import io.realm.Sort
+import java.util.*
+
+class LocalRepository(
+        val realm: Realm
+) : RepositoryContract {
+    override fun create(contact: Contact): Completable {
+        return Completable.create {
+            contact.id = UUID.randomUUID().toString()
+            contact.create()
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun read(): Single<List<Contact>> {
+        return Single.create {
+            emitter ->
+            val list = Contact().querySorted("firstName", Sort.ASCENDING)
+            emitter.onSuccess(list)
+        }
+    }
+
+    override fun update(contact: Contact): Completable {
+        return Completable.create {
+            contact.save()
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun delete(id: String): Completable {
+        return Completable.create {
+            Contact().delete {
+                it.equalTo("id", id)
+            }
+        }.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+    }
+
+    override fun getContact(contactId: String): Single<Contact> {
+       return Single.create {
+           emitter ->
+           val contact = Contact().queryFirst { it.equalTo("id", contactId) }
+
+           contact?.let {
+               emitter.onSuccess(it)
+           }
+           emitter.onSuccess(Contact())
+       }
+    }
+
+    override fun search(query: String): Single<List<Contact>> {
+        return Single.create {
+            val results = realm.where(Contact::class.java)
+                    .contains("name", query)
+                    .or()
+                    .contains("lastname", query)
+                    .or()
+                    .contains("phonenumbers", query)
+                    .findAllSorted("name")
+
+            val list: List<Contact> = realm.copyFromRealm(results)
+            it.onSuccess(list)
+        }
+    }
+}
