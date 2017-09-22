b02b558bbc515d14f24184ff8eae7551a1da3993
Paul Nunez <paulnunezm@gmail.com>
2017-09-22 00:30:16 -0400

    Added: The implementation needed to display both favorite and normal contact list.

--- app/src/main/java/com/nunez/androidcodetestpaulnunez/screens/contactList/ListActivity.kt
+++ app/src/main/java/com/nunez/androidcodetestpaulnunez/screens/contactList/ListActivity.kt
@@ -3,26 +3,35 @@ package com.nunez.androidcodetestpaulnunez.screens.contactList
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.app.ActivityOptionsCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import com.nunez.androidcodetestpaulnunez.R
 import com.nunez.androidcodetestpaulnunez.entities.Contact
+import com.nunez.androidcodetestpaulnunez.repository.LocalRepository
+import com.nunez.androidcodetestpaulnunez.repository.RepositoryContract
 import com.nunez.androidcodetestpaulnunez.screens.addEditContact.AddEditActivity
 import com.nunez.androidcodetestpaulnunez.screens.contactDetails.DetailsActivity
 import com.nunez.androidcodetestpaulnunez.screens.searchContact.SearchActivity
+import com.nunez.androidcodetestpaulnunez.views.ContactListAdapter
+import io.realm.Realm
+import kotlinx.android.synthetic.main.content_contact_list.*
 import kotlinx.android.synthetic.main.list_contact_activity.*
 
 class ListActivity : AppCompatActivity(), ListContract.View {
 
-
     lateinit var presenter: ListContract.Presenter
     lateinit var interactor: ListContract.Interactor
+    lateinit var repository: RepositoryContract
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_contact_activity)
-        setSupportActionBar(toolbar)
+
+        val realm = Realm.getDefaultInstance()
+        repository = LocalRepository(realm)
+        interactor = ListInteractor(this, repository)
+        presenter = ListPresenter(this, interactor)
 
         toolbar.apply {
             title = "Search contact"
@@ -33,7 +42,12 @@ class ListActivity : AppCompatActivity(), ListContract.View {
 
         fab.setOnClickListener { }
 
+        val layoutManager = LinearLayoutManager(this)
+        val FavoritesLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        contactsRecycler.layoutManager = layoutManager
+        favoritesRecycler.layoutManager = FavoritesLayoutManager
 
+        presenter.requestContacts()
     }
 
     override fun addContactClickListener() {
@@ -53,7 +67,29 @@ class ListActivity : AppCompatActivity(), ListContract.View {
     }
 
     override fun showContacts(contacts: List<Contact>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val favoriteContacts = contacts.filter {
+            it.favorite
+        }
+
+        val adapter = ContactListAdapter(contacts, false, {
+
+        }, {
+
+        })
+
+        val favoritesAdapter = ContactListAdapter(favoriteContacts, true, {
+
+        }, {
+
+        })
+
+        contactsRecycler.adapter = adapter
+        favoritesRecycler.adapter = favoritesAdapter
+
+    }
+
+    override fun showEmtpyScreen() {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun showOptionsModalBottomSheet(id: String) {
@@ -61,7 +97,7 @@ class ListActivity : AppCompatActivity(), ListContract.View {
     }
 
     override fun showErrorMessage(message: String) {
-        Snackbar.make(container, message, Snackbar.LENGTH_SHORT).show()
+//        Snackbar.make(container, message, Snackbar.LENGTH_SHORT).show()
     }
 
     override fun dismissModal() {
@@ -83,7 +119,7 @@ class ListActivity : AppCompatActivity(), ListContract.View {
         startActivity(intent)
     }
 
-    override fun goToSearchActivity(contactId: String) {
+    override fun goToSearchActivity() {
         val intent = Intent(this, SearchActivity::class.java)
         val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, toolbar, "toolbar")
 
